556396c3dff9125cf1be5f36d1e377c9ea6bb687
Sylvain Blondeau <sylvain.blondeau@wildcodeschool.com>
2022-09-15 15:43:01 +0200

    poo4 start

--- src/Animal.php
+++ src/Animal.php
@@ -7,4 +7,20 @@ class Animal
     public bool $carnivorous = false;
     public int $pawNumber;
     public string $threatenedLevel = 'NE';
+
+    public function speak($lang = 'fr'): string
+    {
+        if ($lang === 'fr') {
+            $message = 'Bienvenue au zoo, je suis un ';
+        } else {
+            $message = 'Welcome to the zoo, I\'am a ';
+        }
+
+        return $message . $this->name;
+    }
+
+    public function isDangerous(): bool
+    {
+        return $this->size > 50 && $this->carnivorous === true;
+    }
 }
