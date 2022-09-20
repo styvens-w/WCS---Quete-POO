c01a0a14bd6772062d5e72b2b957f65efba94aec
Sylvain Blondeau <sylvain.blondeau@wildcodeschool.com>
2022-09-20 12:04:13 +0200

    poo5 start

--- public/index.php
+++ public/index.php
@@ -5,23 +5,16 @@
 
 require __DIR__ . '/../src/Animal.php';
 
-$lion = new Animal();
-$lion->name = 'lion';
-$lion->pawNumber = 4;
-$lion->carnivorous = true;
-$lion->size = 70;
-$lion->threatenedLevel = 'VU';
+$lion = new Animal('lion', 4);
+$lion->setCarnivorous(true);
+$lion->setSize(70);
+$lion->setThreatenedLevel('VU');
 
-$parrot = new Animal();
-$parrot->name = 'parrot';
-$parrot->pawNumber = 2;
-$parrot->carnivorous = true;
-$parrot->size = 30;
+$parrot = new Animal('parrot', 2);
+$parrot->setSize(30);
 
-$elephant = new Animal();
-$elephant->name = 'elephant';
-$elephant->pawNumber = 4;
-$elephant->threatenedLevel = 'LC';
+$elephant = new Animal('elephant', 4);
+$elephant->setThreatenedLevel('LC');
 
 $animals = [$lion, $parrot, $elephant];
 
--- src/Animal.php
+++ src/Animal.php
@@ -2,11 +2,69 @@
 
 class Animal
 {
-    public string $name;
-    public float $size = 100;
-    public bool $carnivorous = false;
-    public int $pawNumber;
-    public string $threatenedLevel = 'NE';
+    private string $name;
+    private float $size = 100;
+    private bool $carnivorous = false;
+    private int $pawNumber;
+    private string $threatenedLevel = 'NE';
+
+    public function __construct(string $name, int $pawNumber)
+    {
+        $this->name = $name;
+        $this->setPawNumber($pawNumber);
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getSize(): float
+    {
+        return $this->size;
+    }
+
+    public function setSize(float $size): void
+    {
+        if ($size < 0) {
+            $size = 0;
+        }
+
+        $this->size = $size;
+    }
+
+    public function getPawNumber(): int
+    {
+        return $this->pawNumber;
+    }
+
+    private function setPawNumber(int $pawNumber): void
+    {
+        if ($pawNumber < 0) {
+            $pawNumber = 0;
+        }
+        $this->pawNumber = $pawNumber;
+    }
+
+    public function getThreatenedLevel(): string
+    {
+        return $this->threatenedLevel;
+    }
+
+    public function setThreatenedLevel(string $threatenedLevel): void
+    {
+        $this->threatenedLevel = $threatenedLevel;
+    }
+
+    public function isCarnivorous(): bool
+    {
+        return $this->carnivorous;
+    }
+
+    public function setCarnivorous(bool $carnivorous): void
+    {
+        $this->carnivorous = $carnivorous;
+    }
 
     public function speak($lang = 'fr'): string
     {
