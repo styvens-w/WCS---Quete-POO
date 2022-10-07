0f44e71c59145a8a4062182e4a7ddefb6de80958
Sylvain Blondeau <sylvain.blondeau@wildcodeschool.com>
2022-10-07 17:28:58 +0200

    update map

--- public/map.php
+++ public/map.php
@@ -2,7 +2,7 @@
 
 use App\Area;
 
-if (!empty($areas) && class_exists(Area::class)) : ?>
+if (class_exists(Area::class)) : ?>
     <input hidden type="checkbox" id="folded-map">
     <label class="folded-map" for="folded-map">
         <img src="assets/images/folded_map.png" alt="map">
@@ -13,7 +13,7 @@ if (!empty($areas) && class_exists(Area::class)) : ?>
         <?php for ($i = 0; $i < 3; $i++) : ?>
             <div class="zone">
                 <img class="additional-image" src="assets/images/<?= $bottomImages[$i] ?>.png" alt="">
-                <?php foreach ($areas as $key => $area) : ?>
+                <?php foreach ($areas ?? [] as $key => $area) : ?>
                     <?php if (method_exists($area, 'getName')) : ?>
                         <?php if ($key % 3 === $i) : ?>
                             <div class="plan-area">
