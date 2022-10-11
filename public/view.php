7bc32364fa97fda6a1867d8fcaddd43ca6cfd3f4
Sylvain Blondeau <sylvain.blondeau@wildcodeschool.com>
2022-10-11 17:35:57 +0200

    heritage affichage

--- public/view.php
+++ public/view.php
@@ -35,8 +35,17 @@ $areaType = $_GET['area'] ?? '';
             }
             ?>
             <?php foreach ($animals ?? [] as $key => $animal) : ?>
+
                 <article>
-                    <?php if (method_exists($animal, 'speak')) : ?>
+                    <aside>
+                        <div class="parents">
+                            <div><?= preg_replace('/.+\\\(.+)$/', '\1', get_class($animal)) ?></div>
+                            <?php foreach (class_parents($animal) ?? [] as $parent) : ?>
+                                <div>↓</div>
+                                <div><?= preg_replace('/.+\\\(.+)$/', '\1', $parent) ?></div>
+                            <?php endforeach; ?>
+                        </div>
+                    </aside> <?php if (method_exists($animal, 'speak')) : ?>
                         <div class="hello">
                             <?= $animal->speak() ?>
                         </div>
