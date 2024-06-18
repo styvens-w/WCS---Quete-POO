4537e4d77230d9f6ae2083af8a4f131f8a2f2970
styvens-w <styvens.wentz@gmail.com>
2024-06-18 11:38:31 +0200

    Done

--- src/Animal.php
+++ src/Animal.php
@@ -2,12 +2,79 @@
 
 class Animal
 {
-    public string $name;
-    public float $size = 100;
-    public bool $carnivorous = false;
-    public int $pawNumber;
-    public string $threatenedLevel = 'NE';
+    // PROPERTIES
+    private string $name;
+    private float $size = 100;
+    private bool $carnivorous = false;
+    private int $pawNumber;
+    private string $threatenedLevel = 'NE';
 
+
+    // CONSTRUCT
+    public function __construct(string $name, int $pawNumber)
+    {
+        $this->name = $name;
+        $this->setPawNumber($pawNumber);
+    }
+
+
+    // GETTERS
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getSize(): float
+    {
+        return $this->size;
+    }
+
+    public function isCarnivorous(): bool
+    {
+        return $this->carnivorous;
+    }
+
+    public function getPawNumber(): int
+    {
+        return $this->pawNumber;
+    }
+
+    public function getThreatenedLevel(): string
+    {
+        return $this->threatenedLevel;
+    }
+
+
+
+    // SETTERS
+    public function setSize(int $size): void
+    {
+        if($size < 1) {
+            $size = 1;
+        }
+        $this->size = $size;
+    }
+
+    public function setCarnivorous(bool $carnivorous): void
+    {
+        $this->carnivorous = $carnivorous;
+    }
+
+    private function setPawNumber(int $pawNumber): void
+    {
+        if($pawNumber < 0) {
+            $pawNumber = 0;
+        }
+        $this->pawNumber = $pawNumber;
+    }
+
+    public function setThreatenedLevel(string $threatenedLevel): void
+    {
+        $this->threatenedLevel = $threatenedLevel;
+    }
+
+
+    // METHODS
     public function speak(string $lang = 'fr'): string
     {
         if ($lang === 'fr') {
